77bddb0f999d09643a926698d95a1594cf173fb7
James Nobles <123Jimmyn@gmail.com>
2024-10-17 00:05:36 -0400

    Correct duplicate END key in function layer

--- keyboards/planck/keymaps/jnobles/keymap.c
+++ keyboards/planck/keymaps/jnobles/keymap.c
@@ -97,14 +97,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------+------+------+------+------+------+------|
  * | XXXX | F1   | F2   | F3   | F4   | XXXX | XXXX | XXXX | XXXX |ScrlDn|ScrlUp| XXXX |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * |      |      |      |      | XXXX |      |      | XXXX | End  |Pg Dn |Pg Up | Home |
+ * |      |      |      |      | XXXX |      |      | XXXX | Home |Pg Dn |Pg Up | End  |
  * `-----------------------------------------------------------------------------------'
  */
 [_FUNCTION] = LAYOUT_planck_grid(
     KC_PSCR, KC_F9,   KC_F10,  KC_F11,  KC_F12,  XXXXXXX,  KC_BTN1, KC_BTN2, KC_BTN3, XXXXXXX, XXXXXXX, XXXXXXX,
     XXXXXXX, KC_F5,   KC_F6,   KC_F7,   KC_F8,   XXXXXXX,  KC_MS_L, KC_MS_D, KC_MS_U, KC_MS_R, XXXXXXX, XXXXXXX,
     XXXXXXX, KC_F1,   KC_F2,   KC_F3,   KC_F4,   XXXXXXX,  XXXXXXX, XXXXXXX, XXXXXXX, KC_WH_D, KC_WH_U, XXXXXXX,
-    _______, _______, _______, _______, XXXXXXX, _______,  _______, XXXXXXX, KC_END,  KC_PGDN, KC_PGUP, KC_END
+    _______, _______, _______, _______, XXXXXXX, _______,  _______, XXXXXXX, KC_HOME,  KC_PGDN, KC_PGUP, KC_END
 ),
 
 /* Adjust (Lower + Raise)
